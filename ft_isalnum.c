331aa5465fc85387ce9c91c59c8c7a88031fbb2b
amaitou <aitouazghouramine@gmail.com>
2022-10-13 15:26:36 +0100

    added ft_isalnum.c

---
+++ ft_isalnum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 11:28:02 by amait-ou          #+#    #+#             */
+/*   Updated: 2022/10/08 22:25:21 by amait-ou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+
+	This functions returns true if the given character is alphanumeric
+	(either number or alphabet)
+
+	since we have created the ft_isdigit() and ft_isalpha() functions
+	I use them right here to check
+
+*/
+
+#include "libft.h"
+
+int	ft_isalnum(int c)
+{
+	return (ft_isdigit(c) || ft_isalpha(c));
+}
