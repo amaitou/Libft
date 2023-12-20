c81f7f0bc3c90f2cbf67147724209822139273fd
Amine Ait Ouazghour <aitouazghouramine@gmail.com>
2023-12-21 00:10:15 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -15,10 +15,10 @@
  	"ft_itoa" is the opposite of "ft_atoi" because "ft_itoa" converts the number
 	from integer to string.
 	
-	first of all let's count how many digits within "nb", that's what
+	first of all, let's count how many digits are within "nb", that's what
 	the function "len_num(long int nb)" does as it checks if "nb"
-	is negative to consider the sign '-' or "0" as a characters otherwise
-	it counts how many digits by dividing the "nb" with 10 and
+	is negative to consider the sign '-' or "0" as characters otherwise
+	it counts how many digits by dividing the "nb" by 10 and
 	returns the result
 
 	why "long int" and not "int"?
@@ -26,19 +26,19 @@
 		is equal or greater than "-2147483647" because "int" is (32-bit) and "long"
 		is (64-bit)
 	
-	ft_itoa creates a variable "nb" to allocate "n" in an enough memory
+	ft_itoa creates a variable "nb" to allocate "n" in enough memory
 	then allocates the proper size based on the result that comes from 
-	"len_num(int n)" and if the allocations fails "NULL" will be returned.
+	"len_num(int n)" and if the allocations fail "NULL" will be returned.
 
 	the function returns a string so putting "NULL Character" is necessary to
-	indicate the end of string that's why we put it in "p[i]" since "i" holds
+	indicate the end of the string that's why we put it in "p[i]" since "i" holds
 	the size of the allocated memory.
 
 	in case "n" == 0 only one memory will be allocated and will hold 0.
 
 	if nb < 0 I put '-' at the beginning of the string because '-' has been
 	counted as a character in "len_num(long int nb)" then I convert the "nb" to 
-	positive integer to perform the operation of converting each digit into 
+	positive integer to operate for converting each digit into 
 	character starting from the end of "n".
 
  
