d672db62eaf4d70d5420eacbb6ca826d7ac78fff
Amine Ait Ouazghour <aitouazghouramine@gmail.com>
2023-12-21 00:07:43 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,12 +14,12 @@
  
  	"ft_bzero" zeroes the given memory according to "n" bytes which is the number 
 	of blocks to be zeroed.
-	the function takes a void pointer which must be casted to "unsigned
+	the function takes a void pointer which must be cast to "unsigned
    	char" since the "ft_bzero" assigns zeroes to the memory byte per byte.
 
-	looping over each block of memory and assign it with zero
+	looping over each block of memory and assigning it zero
 	is a traditional way but using ft_memset which performs the same operation
-	is more helpful and provide less code.
+	is more helpful and provides less code.
 
  */
 
